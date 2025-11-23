eb59ccbdd0d765d2f1b4e1116143a23a495a1c97
DotSlashGabut <dotslashgabut@gmail.com>
2025-11-23 21:15:41 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,17 +20,23 @@ app.use(express.json({ limit: '50mb' }));
 
 // Rate Limiting
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 1000, // Limit each IP to 1000 requests per windowMs
     standardHeaders: true,
     legacyHeaders: false,
 });
 app.use(limiter);
 
 const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 5 * 60 * 1000, // 5 minutes
     max: 5, // Limit each IP to 5 login requests per windowMs
-    message: 'Too many login attempts from this IP, please try again after 15 minutes'
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: (req, res) => {
+        res.status(429).json({
+            error: 'Too many login attempts from this IP, please try again after 15 minutes'
+        });
+    }
 });
 
 // Sync database
